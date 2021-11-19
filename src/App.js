b3a2306a9f30bc1e2bde7a3eb01dbb2d9c904e9f
MrP29 <pokkm29@gmail.com>
2021-11-18 23:18:05 -0500

    Update_Initial_Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course1" />
+    </div>
+  );
+}
+
+export default App;
